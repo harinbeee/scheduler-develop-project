0fdc398e0f94b9fc6d98c1389d9d83516971c22a
harinbeee <luv_lyn@naver.com>
2025-04-03 19:14:53 +0900

    Feat: user,schedule 엔티티 설계

---
+++ src/main/java/com/example/schedulerproject/entity/Schedule.java
@@ -0,0 +1,41 @@
+package com.example.schedulerproject.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table (name = "Schedule")
+public class Schedule extends BaseEntity {
+
+//    **필드**
+    // ID 자동생성 , PK 지정하기
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 할일 제목
+    @Column(nullable = false)
+    private String title;
+
+    // 할일 내용
+    @Column(columnDefinition = "longtext")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+//    **생성자**
+    // 기본 생성자
+    public Schedule() {
+    }
+
+    // 일정 생성자
+    public Schedule(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    // 작성한 유저 이름 붙이기
+}
---
+++ src/main/java/com/example/schedulerproject/entity/User.java
@@ -0,0 +1,38 @@
+package com.example.schedulerproject.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "user")
+public class User extends BaseEntity {
+
+//    **필드**
+    // ID 자동생성 , PK 지정하기
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 유저명
+    @Column(nullable = false)
+    private String username;
+
+    // 유저 이메일
+    @Column(nullable = false)
+    private String mail;
+
+    // 유저 비밀번호
+    @Column(nullable = false)
+    private String password;
+
+//    ** 생성자**
+    public User() {
+    }
+
+    public User(String username, String mail, String password) {
+        this.username = username;
+        this.mail = mail;
+        this.password = password;
+    }
+}
